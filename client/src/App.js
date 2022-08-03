771f66c39cc6565fb95959f0faba498d4f6f31e6
Ayushi Agarwal <ayushiagg6789@gmail.com>
2022-08-03 23:14:00 +0530

    todos

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Tasklist from './Components/taskList';
+
+function App(){
+  return ( 
+    <> 
+      <div className="App">
+        <Tasklist />
+      </div>
+    
+    </>
+  );
+}
+
+export default App;
